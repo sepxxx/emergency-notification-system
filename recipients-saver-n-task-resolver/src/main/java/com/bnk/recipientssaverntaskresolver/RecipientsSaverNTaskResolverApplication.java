f4b8eafa465c927a26abc77ba2b3e3b2101701e0
sepxx <mihartthx@gmail.com>
2023-11-13 15:18:42 +0300

    init

---
+++ recipients-saver-n-task-resolver/src/main/java/com/bnk/recipientssaverntaskresolver/RecipientsSaverNTaskResolverApplication.java
@@ -0,0 +1,13 @@
+package com.bnk.recipientssaverntaskresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipientsSaverNTaskResolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipientsSaverNTaskResolverApplication.class, args);
+    }
+
+}
---
+++ recipients-saver-n-task-resolver/src/test/java/com/bnk/recipientssaverntaskresolver/RecipientsSaverNTaskResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnk.recipientssaverntaskresolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipientsSaverNTaskResolverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
