dff9a8b58a5a8b2fcbecf3ff864a934665d9ad2c
sepxx <mihartthx@gmail.com>
2023-11-18 13:56:26 +0300

    gradle problem solved

--- notification-sender/src/main/java/com/bnk/notificationsender/configs/ImportConfig.java
+++ notification-sender/src/main/java/com/bnk/notificationsender/configs/ImportConfig.java
@@ -1,5 +1,6 @@
 package com.bnk.notificationsender.configs;
 
+import com.bnk.miscellaneous.EnableStore;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
